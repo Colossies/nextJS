9fd5b4bdce8a8bb240d095d40ee07aaf4c0cee75
Colossies <tjen.bryanchandra@gmail.com>
2025-04-15 15:22:36 +0700

    another attempt at tailwind

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  basePath: isProd ? "/nextJs": "",
-  assetPrefix: isProd ? '/your-repo-name/' : '',
+  basePath: "/nextJs",
+  assetPrefix: './',
   output: "export",
   reactStrictMode: true,
 };
