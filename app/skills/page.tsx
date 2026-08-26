e21008d386ea26917f1faf03c60cabf0ca269edc
colossies <tjen.bryanchandra@gmail.com>
2026-08-26 09:53:09 +0800

    missed still importing the now deleted skills_minor

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,7 +1,7 @@
 
 import Image from 'next/image';
 import {Skills as SkillsMajor} from "./sections/skills_major";
-import {Skills as SkillsMinor} from "./sections/skills_minor";
+
 export default function Page() {
     return (
     <>
